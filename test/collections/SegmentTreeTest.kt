6ea769948f8b7c66a5558300bee79b2902f723b1
mban259 <mban259@users.noreply.github.com>
2020-02-01 17:07:21 +0900

    追加 SegmentTree

---
+++ test/collections/SegmentTreeTest.kt
@@ -0,0 +1,50 @@
+package collections
+
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+import kotlin.test.assertEquals
+
+class SegmentTreeTest {
+    @Test
+    fun randomTest() {
+        val st = SegmentTree()
+        val array = Array(1000) { 0 }
+        val random = Random(0)
+        for (i in 1..100000) {
+            val type = random.nextInt(2)
+            when (type) {
+                0 -> {
+                    // 更新
+                    val index = random.nextInt(1000)
+                    val num = random.nextInt(10000)
+                    array[index] = num
+                    st[index] = num
+                }
+                1 -> {
+                    // 区間演算
+                    val left = random.nextInt(1000)
+                    val right = random.nextInt(left, 1000) + 1
+
+                    var expect = 0
+                    for (j in left until right) {
+                        expect += array[j]
+                    }
+                    val actual = st.query(left, right)
+
+                    assertEquals(expect, actual)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun test1() {
+        val st = SegmentTree()
+        st[0] = 4
+        st[1] = 3
+        st[2] = 2
+        st[3] = 1
+        st[4] = 0
+        assertEquals(3, st.query(2, 5))
+    }
+}
